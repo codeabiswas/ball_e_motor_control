f1b6649357f75bd37a2f8cdc473c7a33d73fa5d3
Andrei Biswas <axb6972@rit.edu>
2021-04-10 19:55:54 -0400

    Updated BQM

--- src/motor_ball_queue.py
+++ src/motor_ball_queue.py
@@ -1,5 +1,10 @@
+import time
+
+import Jetson.GPIO as gpio
+
+
 class MotorBallQueue:
- """The BQM Motor will be controlled using the 'Move to Incremental Distance (1 Distance, Home To Switch)' Setting. As Teknik puts it, 
+    """The BQM Motor will be controlled using the 'Move to Incremental Distance (1 Distance, Home To Switch)' Setting. As Teknik puts it,
     'this mode was designed for replacing hydraulic or pnewmatic cylinders that move between two positions'
     """
 
@@ -27,29 +32,23 @@ class MotorBallQueue:
         # This variable will track whether or not the motor is energized
         self.motor_on = False
 
-        # This variable will store the position of the motor (By default, it should be at pos. 1)
-        self.bqm_pos = 0
-    
     def energize_motor(self):
         """Turns the motor on
         """
         # Set Enable pin to high to energize motor
         gpio.output(self.en_pin, gpio.HIGH)
-        
-        #Initialize pos
-        self.bqm_pos = 0
+
         # Motor is now energized
-        self.motor_on = True    
-    
-    def move_forward(self):
-        """Feed moves forward (pos. 2)
+        self.motor_on = True
+
+    def turn_once(self):
+        """BQM turns a rotation which allows one ball to fall
         """
 
-        # Set Input A to high to move to position 2
+        # Set Input A to high to move
         gpio.output(self.in_a_pin, gpio.HIGH)
-
-        #Increment pos
-        self.bqm_pos += 1   
+        time.sleep(0.5)
+        gpio.output(self.in_a_pin, gpio.LOW)
 
     def get_motor_state(self):
         """Returns whether or not the motor is energized
@@ -58,28 +57,23 @@ class MotorBallQueue:
         """
         return self.motor_on
 
-    def get_pos(self):
-        """Returns the current position of the feed motor
-        Returns:
-            self.bfm_pos [int]: Number is analogous to position (1 -> pos. 1 and 2 -> pos. 2)
-        """
-        return self.bqm_pos
-
     def stop_and_reset_motor(self):
         """Stops the motor and resets all previously set values to their default values
         """
-        
-        #Turn off motor
-        gpio.output(self.en_pin, gpio.LOW)
+
         # Set Input A to low to move to position 1
         gpio.output(self.in_a_pin, gpio.LOW)
 
+        # Turn off motor
+        gpio.output(self.en_pin, gpio.LOW)
+
         # Clean all BFM-related channels
         # NOTE: Doing this means the pins have been set to their default state, and init method needs to be called again to make this motor work
-        gpio.cleanup(self.pm_channels)
+        gpio.cleanup(self.bqm_channels)
 
         # Motor is not energized
         self.motor_on = False
+
 
 def main():
     # Initialize object
@@ -89,17 +83,24 @@ def main():
     # Get energized state of motor
     print(motor_bqm.get_motor_state())
     # Move motor forwards
-    motor_bqm.move_forward()
+    motor_bqm.turn_once()
     # Get position
     print(motor_bqm.get_pos())
+    time.sleep(2)
     # Move motor forwards
-    motor_bqm.move_forward()
+    motor_bqm.turn_once()
     # Get position
-    print(motor_bqm.get_pos())    
+    print(motor_bqm.get_pos())
+    time.sleep(2)
+    # Move motor forwards
+    motor_bqm.turn_once()
+    # Get position
+    print(motor_bqm.get_pos())
     # Reset motor
     motor_bqm.stop_and_reset_motor()
     # Get energized state of motor
     print(motor_bqm.get_motor_state())
+
 
 if __name__ == "__main__":
     main()
